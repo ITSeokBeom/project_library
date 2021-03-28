78cb6c69ace9c4599a09e8910b2c1a47188a7823
ITSeokBeom <79242459+ITSeokBeom@users.noreply.github.com>
2021-03-28 22:40:49 +0900

    2021.03.28[1.테스트]

--- project_library_git/src/main/java/com/example/service/NoticeService.java
+++ project_library_git/src/main/java/com/example/service/NoticeService.java
@@ -3,7 +3,6 @@ package com.example.service;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
